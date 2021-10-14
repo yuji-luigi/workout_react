5040003769ed1042d5887105dfece56024c76d90
Yuji Sato <yujisato@192.168.1.4>
2021-10-14 18:28:03 +0200

    heroku deployment

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "frontend/public")));
+// Set static folder
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 4242;
 app.listen(PORT, () =>
