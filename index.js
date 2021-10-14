e32d2a809464117b0ed53c1640c12db2071bbdb4
Yuji Sato <yujisato@192.168.1.4>
2021-10-14 21:55:48 +0200

    deployment

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 
 // Set static folder
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
   });
 }
 
