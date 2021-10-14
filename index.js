7d592289f41ab6351a0b5c355a443ec9c06837c2
Yuji Sato <yujisato@192.168.1.4>
2021-10-14 18:43:02 +0200

    deploy heroku

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "frontend", "public/build", "index.html")
+    );
   });
 }
 
