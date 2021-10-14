64313d9218cac11d10145ecaa7e3f010a6f6d82f
Yuji Sato <yujisato@192.168.1.4>
2021-10-14 16:07:27 +0200

    heroku deploy

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname, "frontend/public")));
+
 const PORT = process.env.PORT || 4242;
 app.listen(PORT, () =>
   console.log(`server is runnning http://localhost:${PORT}`)
